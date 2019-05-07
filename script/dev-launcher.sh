83a12cb84ea477f83a5a2af717e6f962c958c68a
Gonçalo Pedro <gpedro34@gmail.com>
2019-05-07 13:22:43 +0100

    opens up an incognito window at the end of the script for testing

---
+++ script/dev-launcher.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+docker-compose down \
+  && docker-compose build \
+  && docker-compose up -d \
+  && docker-compose start wait \
+  && docker-compose restart brs-crawler \
+  && docker-compose restart utils-crawler \
+  && echo "Everything is up and running!" \
+  && start chrome --incognito
